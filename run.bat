4235f19213c6db11e634cc6b153d0d201d3da191
EthanMash0 <ethanlmash@gmail.com>
2026-06-29 17:44:49 -0400

    fix: spacing issue between arguments

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -Verb RunAs -WorkingDirectory '%~dp0' -ArgumentList'-NoProfile -ExecutionPolicy Bypass -File ""%~dp0BackupBench.ps1""'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -Verb RunAs -WorkingDirectory '%~dp0' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0BackupBench.ps1""'"

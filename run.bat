cf9a0a10cffda932a775f074a581688fb6712ed1
EthanMash0 <ethanlmash@gmail.com>
2026-06-29 17:32:50 -0400

    fix: update run.bat to spawn an elevated command prompt and remove top level tool folder

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process
+powershell.exe -Verb RunAs -WorkingDirectory '%~dp0' -ArgumentList
+'-NoProfile -ExecutionPolicy Bypass -File ""%~dp0BackupBench.ps1""'"
